1eab80a95fd655a4a0cb6c96d8cb720d4daa83e0
Zehua <zl8020@princeton.edu>
2022-11-08 06:49:52 -0500

    Create a main function to call other functions

---
+++ main.m
@@ -0,0 +1,24 @@
+%% Foldername
+Folder_sufs = {'Re38L8','Re38L9','Re55L8','Re55L9','Re77L8','Re77L9','Re150L9'};
+
+
+%% Parameter space
+wes = [3,4,5,6,7,8,9,10];
+mus = [0.01,0.1,1,10,50,100,150];
+ts = [80,90,100,110,120,130,140,150,160,170];
+
+%% Excel for breakupinfo
+%% Normal folder
+% for i_folder = 1:numel(Folder_sufs)
+% populate_breakup_boolean(i_folder,Folder_sufs,wes,mus,ts)
+% end
+
+%% Size folder
+% i_folder = 6;
+% wes=4;
+% mus = [0.01 10];
+% ts = 60:3:168;
+% populate_breakup_boolean(i_folder,Folder_sufs,wes,mus,ts)
+
+
+
