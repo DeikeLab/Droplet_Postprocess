5999e80813a98fae5fb6a0ca9e1edd1813660975
Zehua <zl8020@princeton.edu>
2022-11-10 11:10:14 -0500

    remove to backup folder

--- trees.m
+++
@@ -1,8 +0,0 @@
-foldernameL8='G:\.shortcut-targets-by-id\1yy0LEVWRsT1lGxOaqULoHgajAkevh3h0\BASF\dist_Re55L8\';
-foldernameL9='G:\.shortcut-targets-by-id\1yy0LEVWRsT1lGxOaqULoHgajAkevh3h0\BASF\dist_Re55L9\';
-Re77dist;
-
-clear
-foldernameL8='G:\.shortcut-targets-by-id\1yy0LEVWRsT1lGxOaqULoHgajAkevh3h0\BASF\dist_Re77L8\';
-foldernameL9='G:\.shortcut-targets-by-id\1yy0LEVWRsT1lGxOaqULoHgajAkevh3h0\BASF\dist_Re77L9\';
-Re77dist;
