42112e867a1570e5bbae190dd029e0813baa92f2
Zehua <zl8020@princeton.edu>
2022-11-10 09:37:24 -0500

    To do update

--- plot_breakup_time.m
+++ plot_breakup_time.m
@@ -1,31 +1,35 @@
-function plot_breakup_time(T,col,mar,sz,fcol,filename,ls)
+function plot_breakup_time(T,col,mar,sz,fcol,filename,ls,Imu)
 
 figure(2)
 %     clf
 %     T(1:end,4) = T(1:end,4)./T(1:end,1).^(1/3);
 %       T(1:end,5) = T(1:end,5)./T(1:end,1).^(1/3);
-    mu = T(1:7:end,1); we = T(1:7:end,2); tb = T(1:7:end,4); std = T(1:7:end,5);
-    errorbar(we,tb,std,'Color',[1,0.1,0],'LineWidth',2,'LineStyle',ls); hold on;
-    
-    mu = T(2:7:end,1); we = T(2:7:end,2); tb = T(2:7:end,4); std = T(2:7:end,5);
-    errorbar(we,tb,std,'Color',[0,1,0],'LineWidth',2,'LineStyle',ls); hold on;
-    
-    mu = T(3:7:end,1); we = T(3:7:end,2); tb = T(3:7:end,4); std = T(3:7:end,5);
-    errorbar(we,tb,std,'Color',[0,0,1],'LineWidth',2,'LineStyle',ls); hold on;
-%     
-    mu = T(4:7:end,1); we = T(4:7:end,2); tb = T(4:7:end,4); std = T(4:7:end,5);
-    errorbar(we,tb,std,'Color',[1,0.3,0],'LineWidth',2,'LineStyle',ls); hold on;
-    
-    mu = T(5:7:end,1); we = T(5:7:end,2); tb = T(5:7:end,4); std = T(5:7:end,5);
-    errorbar(we,tb,std,'Color',[1,0.5,0],'LineWidth',2,'LineStyle',ls); hold on;
-    
-    mu = T(6:7:end,1); we = T(6:7:end,2); tb = T(6:7:end,4); std = T(6:7:end,5);
-    errorbar(we,tb,std,'Color',[1,0.7,0],'LineWidth',2,'LineStyle',ls); hold on;
-     mu = T(7:7:end,1); we = T(7:7:end,2); tb = T(7:7:end,4); std = T(7:7:end,5);
-    errorbar(we,tb,std,'Color',[1,0.9,0],'LineWidth',2,'LineStyle',ls); hold on;
 
-    legend('$\mu=0.01$','$\mu=0.1$','$\mu=1$','$\mu=10$','$\mu=50$','$\mu=100$','$\mu=150$', ...
-        'Location','northeastoutside','Interpreter','latex');
+    Colors={[1,0,0],[0,1,0],[0,0,1],[1,0.3,0],[1,0.5,0],[1,0.7,0],[1,0.9,0]}
+    for I=Imu
+    mu = T(I:7:end,1); we = T(I:7:end,2); tb = T(I:7:end,4); std = T(I:7:end,5);
+    errorbar(we,tb,std,'Color',Colors{I},'LineWidth',2,'LineStyle',ls); hold on;
+    end
+    
+%     mu = T(2:7:end,1); we = T(2:7:end,2); tb = T(2:7:end,4); std = T(2:7:end,5);
+%     errorbar(we,tb,std,'Color',[0,1,0],'LineWidth',2,'LineStyle',ls); hold on;
+%     
+%     mu = T(3:7:end,1); we = T(3:7:end,2); tb = T(3:7:end,4); std = T(3:7:end,5);
+%     errorbar(we,tb,std,'Color',[0,0,1],'LineWidth',2,'LineStyle',ls); hold on;
+% %     
+%     mu = T(4:7:end,1); we = T(4:7:end,2); tb = T(4:7:end,4); std = T(4:7:end,5);
+%     errorbar(we,tb,std,'Color',[1,0.3,0],'LineWidth',2,'LineStyle',ls); hold on;
+%     
+%     mu = T(5:7:end,1); we = T(5:7:end,2); tb = T(5:7:end,4); std = T(5:7:end,5);
+%     errorbar(we,tb,std,'Color',[1,0.5,0],'LineWidth',2,'LineStyle',ls); hold on;
+%     
+%     mu = T(6:7:end,1); we = T(6:7:end,2); tb = T(6:7:end,4); std = T(6:7:end,5);
+%     errorbar(we,tb,std,'Color',[1,0.7,0],'LineWidth',2,'LineStyle',ls); hold on;
+%      mu = T(7:7:end,1); we = T(7:7:end,2); tb = T(7:7:end,4); std = T(7:7:end,5);
+%     errorbar(we,tb,std,'Color',[1,0.9,0],'LineWidth',2,'LineStyle',ls); hold on;
+
+    legends = {'$\mu=0.01$','$\mu=0.1$','$\mu=1$','$\mu=10$','$\mu=50$','$\mu=100$','$\mu=150$'};
+    legend(legends{Imu},'Location','northeastoutside','Interpreter','latex');
     % legend('Re 150 L9','Location','northeast');
     
     xlabel('We','Interpreter','latex');
